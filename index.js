810d92c362dd3038e0f5975bc6769b7129bfc11a
block-doc-au <neilc@cns-gc.com.au>
2022-09-16 21:29:13 +1000

    NC Completed Document OnLoad lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const myChange = document.getElementById('text');
+    myChange.innerHTML = ("This is really cool!")
+});
